332cf888db5b8dc5d4510250c73ad459c6fd0847
HouJuan <130810316+houjuan0623@users.noreply.github.com>
2024-01-03 07:46:09 +0000

    添加token的过期检测

--- server/config/passport.js
+++ server/config/passport.js
@@ -4,7 +4,7 @@ const ExtractJwt = require('passport-jwt').ExtractJwt
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
 const config = require('./config') // 假设您的配置文件中包含了 JWT 密钥
-const UserModel = require('../models/users') // 用户模型
+const { UserModel } = require('../models/index') // 用户模型
 
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // 定义如何从请求中提取 JWT。从请求头中的 Authorization 字段提取以 Bearer 开头的 token。
@@ -14,6 +14,10 @@ const options = {
 passport.use(
   new JwtStrategy(options, async (jwt_payload, done) => {
     try {
+      const currentTime = Math.floor(Date.now() / 1000)
+      if (jwt_payload.exp < currentTime) {
+        return done(null, false, { message: 'token过期' })
+      }
       const userModel = await UserModel.getInstance()
       const user = await userModel.findUserByName(jwt_payload.sub)
       if (user) {
